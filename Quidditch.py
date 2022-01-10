921e8476e02041ba4c72ac44bbc72cd5bc0ed756
cannappin <87528956+cannappin@users.noreply.github.com>
2022-01-10 09:49:04 +0100

    update fonctions

--- Quidditch.py
+++ Quidditch.py
@@ -1,3 +1,7 @@
 
 
-name = str(input("Entrez le nom de la personne"))
+nom = str(input("Entrez le nom de la personne\n"))
+prenom = str(input("Entrez le prénom de la personne\n"))
+annee = int(input("Entrez l'année de naissance de la personne\n"))
+
+
---
+++ fonction_quidditch.py
@@ -0,0 +1,6 @@
+
+
+def Mail(nom , prenom) :
+    return prenom[0].upper() + "." + nom.lower() +"@baton-rouge.fr"
+
+
