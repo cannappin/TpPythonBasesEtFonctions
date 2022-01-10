e54209e7a37045832d7a140d8b040d1009590722
cannappin <87528956+cannappin@users.noreply.github.com>
2022-01-10 09:56:05 +0100

    Update fonction_quidditch.py

--- fonction_quidditch.py
+++ fonction_quidditch.py
@@ -4,3 +4,16 @@ def Mail(nom , prenom) :
     return prenom[0].upper() + "." + nom.lower() +"@baton-rouge.fr"
 
 
+def categorie(annee):
+    if annee < 6 and annee > 40 :
+        return "Non Admis"
+    elif annee < 12 :
+        return "Poussin"
+    elif annee < 18 :
+        return "Cadet"
+    elif annee < 24 :
+        return "Junior"
+    elif annee < 30 :
+        return "Semi-Pro"
+    elif annee <= 40 :
+        return "Pro"
